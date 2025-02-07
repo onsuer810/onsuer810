138bea3e22cd11d76e56f80ffb29eb517b3d57ee
onsuer810 <onsoff@seznam.cz>
2025-02-07 20:30:34 +0100

    Update my_project.js

--- my_project.js
+++ my_project.js
@@ -5,8 +5,7 @@
 //import b4w from "blend4web";
 import * as b4w from "./b4w.js";
 console.log("b4w.js jest");
-console.log("b4 = "+b4w);
-console.log("b4w.math ="+b4w.math);
+
 var m_app = b4w.app;
 var m_cfg = b4w.config;
 var m_data = b4w.data;
@@ -16,7 +15,10 @@ var m_anim = b4w.animation;
 var m_cont = b4w.container;
 var m_ctl = b4w.controls;
 var m_mouse = b4w.mouse;
-var m_math = b4w.math;
+//var m_math = b4w.math;
+const m_math = b4w.math;
+console.log("b4 = "+b4w);
+console.log("b4w.math ="+b4w.math);
 var m_obj = b4w.objects;
 var m_phy = b4w.physics;
 var m_scenes = b4w.scenes;
